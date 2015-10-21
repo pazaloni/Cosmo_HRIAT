37ebcae60ae4c19d4154b7e371c4e718c9663998
cst214 <cst214@L239B04.cst.siast.sk.ca>
2015-10-21 13:50:09 -0600

    Added variables

---
+++ src/TechMainPageGUI.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class TechMainPageGUI {
+
+	private Button viewLog = new Button();
+	private Button addUser = new Button();
+	private Button editUser = new Button();
+	private Button logOut = new Button();
+	private Button removeUser = new Button();
+	
+	private Image logo = null;
+	
+	private Label techTitle = new Label();
+	
+	private Pane managePane = new Pane();
+	
+	public void start(Stage stage) throws Exception
+	{
+		
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
