fe72abee2de2c94fffa5d425690b6e18a8e46cf5
cst203 <cst203@L239B01.cst.siast.sk.ca>
2015-10-21 13:51:43 -0600

    Started medical main page

--- CIMP_Project/src/MedicalStaffMainPageGUI.java
+++ CIMP_Project/src/MedicalStaffMainPageGUI.java
@@ -1,5 +1,109 @@
 
-public class MedicalStaffMainPageGUI
-{
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+public class MedicalStaffMainPageGUI extends Application
+{
+    public static void main(String[] args) {
+        launch();
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        stage.setTitle("Cosmo Industries");
+        
+        VBox root = createMainVBox();
+
+        stage.setScene(new Scene(root, 300, 250));
+        stage.show();
+        
+    }
+    
+    public HBox createHBoxHeader() {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #FFFFFF;");
+
+        Button buttonCurrent = new Button("Log Out");
+        buttonCurrent.setPrefSize(100, 20);
+        
+       // Image logo = new Image(".\\CosmoIconLong[1].png");
+
+        //hbox.getChildren().addAll(logo,buttonCurrent);
+
+        return hbox;
+    }
+    
+    public HBox createHBoxTabs() {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #336699;");
+
+        Button buttonCurrent = new Button("Tabs Go Here");
+        buttonCurrent.setPrefSize(100, 20);
+
+        hbox.getChildren().addAll(buttonCurrent);
+
+        return hbox;
+    }
+    
+    public HBox createHBoxPreviewNotes() {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #336699;");
+
+        Button buttonCurrent = new Button("PreviewNotes Here");
+        buttonCurrent.setPrefSize(100, 20);
+
+        hbox.getChildren().addAll(buttonCurrent);
+
+        return hbox;
+    }
+    
+    public HBox createHBoxTable() {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #336699;");
+
+        Button buttonCurrent = new Button("Table Goes Here");
+        buttonCurrent.setPrefSize(100, 20);
+
+        hbox.getChildren().addAll(buttonCurrent);
+
+        return hbox;
+    }
+
+    
+    public VBox createMainVBox() {
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(10));
+        vbox.setSpacing(8);
+
+        HBox header = createHBoxHeader();
+        HBox tabs = createHBoxTabs();
+        HBox previewNotes = createHBoxPreviewNotes();
+        HBox table = createHBoxTable();
+        vbox.getChildren().addAll(header, tabs, previewNotes, table);
+
+        return vbox;
+    }
+    
 }
