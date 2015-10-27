f1370b437c93e8b17f7e64ee414dc5d79974b2d7
cst203 <cst203@L239B15.cst.siast.sk.ca>
2015-10-27 15:03:11 -0600

    added default picture image

--- CIMP_Project/src/MedicalStaffMainPageGUI.java
+++ CIMP_Project/src/MedicalStaffMainPageGUI.java
@@ -101,7 +101,7 @@ public class MedicalStaffMainPageGUI extends Application
     
     public HBox createHBoxPreviewNotes() {
         HBox hbox = new HBox();
-        hbox.setPadding(new Insets(15, 12, 15, 12));
+       // hbox.setPadding(new Insets(15, 12, 15, 12));
         hbox.setSpacing(10);
         hbox.setStyle("-fx-background-color: #336699;");
 
