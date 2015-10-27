b6cba23a1d49836a5d3d1199ff611837f50b5aa4
cst206 <cst206@L239B11.cst.siast.sk.ca>
2015-10-27 08:48:36 -0600

    Added the logo, proper spacing, window dimensions, textfield placeholder text

--- CIMP/src/LoginGUI.java
+++ CIMP/src/LoginGUI.java
@@ -11,11 +11,25 @@ import javafx.stage.Stage;
 
 public class LoginGUI extends Application
 {
+	/* Login Elements */
 	private VBox mainPane;
 	private TextField userName;
 	private PasswordField userPassword;
 	private Button loginBtn;
 	private Image logo;
+
+	/* Static final variables */
+	public static final int WIDTH = 875;
+	public static final int HEIGHT = 580;
+	public static final int IMAGE_WIDTH = 606;
+	public static final int IMAGE_HEIGHT = 75;
+	public static final int TEXTFIELD_WIDTH = 190;
+	public static final int MAINPANE_SPACING = 10;
+	public static final String USERNAME_PROMPT_TEXT = "Username";
+	public static final String PASSWORD_PROMPT_TEXT = "Password";
+	public static final String STAGE_TITLE = "Cosmopolitan Industries";
+	public static final String IMAGE_PATH = "CosmoIconLong.png";
+	public static final String BUTTON_LABEL = "Login";
 
 	public static void main(String[] args)
 	{
@@ -26,29 +40,40 @@ public class LoginGUI extends Application
 	public void start(Stage stage) throws Exception
 	{
 		initializeVariables();
+		// adding the elements to the mainPane
 		mainPane = createBox();
-		Scene scene = new Scene(mainPane);
 		mainPane.setAlignment(Pos.CENTER);
+
+		// main scene to be displayed
+		Scene scene = new Scene(mainPane);
+		// Styling the scene
 		stage.setScene(scene);
 		stage.setTitle("Cosmopolitan Industries");
-
+		stage.setWidth(WIDTH);
+		stage.setHeight(HEIGHT);
+		stage.setTitle(STAGE_TITLE);
+		stage.setResizable(false);
 		stage.show();
 	}
 
 	/**
-	 * Purpose: Create a login
+	 * Purpose: Create a login box and add the logo, username, and password
+	 * textfield
 	 * 
-	 * @return
+	 * @return a new box with the login elements
 	 */
 	private VBox createBox()
 	{
 		VBox box = new VBox();
+		// Viewable image of the logo
 		ImageView viewableLogo = new ImageView(logo);
-		viewableLogo.maxWidth(500);
-		viewableLogo.setFitWidth(500);
-		viewableLogo.setFitHeight(250);
+		// Setting image dimensions
+		viewableLogo.setFitWidth(IMAGE_WIDTH);
+		viewableLogo.setFitHeight(IMAGE_HEIGHT);
+		// Adding everyting to the mainbox
 		box.getChildren()
 				.addAll(viewableLogo, userName, userPassword, loginBtn);
+		box.setSpacing(MAINPANE_SPACING);
 		return box;
 
 	}
@@ -60,9 +85,20 @@ public class LoginGUI extends Application
 	private void initializeVariables()
 	{
 		userName = new TextField();
+		// Setting the placeholder text in the textfield
+		userName.setPromptText(USERNAME_PROMPT_TEXT);
+		// Setting the maximum width the textfield
+		userName.setMaxWidth(TEXTFIELD_WIDTH);
+
 		userPassword = new PasswordField();
-		loginBtn = new Button("Login");
-		logo = new Image("CosmoIconLong.png");
+		// Setting the placeholder text in the textfield
+		userPassword.setPromptText(PASSWORD_PROMPT_TEXT);
+		// Setting the maximum width the textfield
+		userPassword.setMaxWidth(TEXTFIELD_WIDTH);
+		// making the new button
+		loginBtn = new Button(BUTTON_LABEL);
+		// making the image for the logo to be displayed
+		logo = new Image(IMAGE_PATH);
 
 	}
 }
