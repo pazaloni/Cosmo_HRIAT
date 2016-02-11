4b1730d40bc6f120df3b358f62a9fab5ee45f9d9
CST215 <CST215@L239B04.cst.siast.sk.ca>
2016-02-11 14:11:00 -0600

    Prepping to pull master

--- src/TestViewNotes.java
+++ src/TestViewNotes.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
-
+import javafx.application.Application;
+import javafx.scene.control.TableView;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,22 +12,32 @@ import org.junit.Test;
  */
 public class TestViewNotes 
 {
-
+	private NoteTableViewController nTVCont;
+	
+	
 	@Before
 	public void setUp() throws Exception 
 	{
+		
+		nTVCont = new NoteTableViewController(new TableView());
+		nTVCont.initialize();
 	}
 
+	/**
+	 * Purpose: Test that all notes in the database are reachable from 
+	 * the list in the NoteTableViewController (nTVCont), with the
+	 * noteIDs from 4-10
+	 */
 	@Test
 	public void test() 
 	{
-		fail("Not yet implemented");
+		for(int i = 4; i <=10 ; i++)
+		{
+			//check that the note exists
+			assertTrue(nTVCont.getNoteFromID(i + "") != null);
+			
+		}
 	}
-	
-	@Test
-	public void testNoteID()
-	{
-		fail("Not yet implemented");
-	}
+
 
 }
