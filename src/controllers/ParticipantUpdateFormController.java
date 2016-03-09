fa88a800facef18c842a31dca2ca6e3636d70bfc
CST215 <CST215@L239B06.cst.siast.sk.ca>
2016-03-09 14:38:45 -0600

    Documented code, currently fixing errors.

--- src/controllers/ParticipantUpdateFormController.java
+++ src/controllers/ParticipantUpdateFormController.java
@@ -264,53 +264,7 @@ public class ParticipantUpdateFormController
 
 
 
-	/**
-	 * Purpose: Saves the participants basic information to the database,
-	 * updating the current record
-	 * @param firstName - participants first name
-	 * @param lastName - participants last name
-	 * @param address - participants address
-	 * @param city - participants city
-	 * @param postalCode - participants postal code
-	 * @param phoneNumber - participants phone number
-	 * @param birthDate - participants birth date
-	 * @param sin - participants social insurance number
-	 */
-	public void saveBasicParticipantInformation(String firstName, String lastName,
-			String address, String city, String postalCode, String phoneNumber,
-			String birthDate, String sin)
-	{
-		//connect the database
-		db.connect();
-		
-		//the values to update in the database
-		String[][] values = new String[9][2];
-		values[0][0] = "cosmoID";
-		values[0][1] = "" + cosmoID;
-		values[1][0] = "firstName";
-		values[1][1] = firstName;
-		values[2][0] = "lastName";
-		values[2][1] = lastName;
-		values[3][0] = "address";
-		values[3][1] = address;
-		values[4][0] = "city";
-		values[4][1] = city;
-		values[5][0] = "postalCode";
-		values[5][1] = postalCode;
-		values[6][0] = "phoneNumber";
-		values[6][1] = phoneNumber;
-		values[7][0] = "dateOfBirth";
-		values[7][1] = birthDate;
-		values[8][0] = "socialInsuranceNumber";
-		values[8][1] = sin;
-		
-		//update participant's record
-		db.update(values, "Participant", values[0][1]);
-		
-		//free up database resources
-		db.disconnect();
-		
-	}
+	
 	
 	/**
 	 * Purpose: Save the information for a kin record. If the
@@ -323,13 +277,16 @@ public class ParticipantUpdateFormController
 	 * @param postalCode - the kins postal code
 	 * @param homePhone - the kins home phone number
 	 * @param workPhone - the kins work phone number
+	 * @return - a String containing any error messages produced during
+	 * 	data validation process.
+	 * @author Breanna Wilson CST215
 	 */
-	public void saveKinInformation(String firstName, String lastName, String address,
+	public String saveKinInformation(String firstName, String lastName, String address,
 			String city, String postalCode, String homePhone, String workPhone)
 	{
 		//connect to the database
 		db.connect();
-		
+		String msg = "";
 		//the array of values to compare against
 		String[][] values = new String[7][2];
 
@@ -382,41 +339,51 @@ public class ParticipantUpdateFormController
 		//else, there is no matching record
 		else
 		{
-			//insert the new record into the database
-			db.insert(values, "Kin");
-			
-			//create the string to put into the WHERE clause
-			whereStmt = this.createWhereStatement(values, 2);
-			
-			//check for the kinID of the new record
-			rs = db.select("kinID", "Kin", whereStmt, "");
-			
-			//the information to update the participant table with
-			kinInfo = new String[2][2];
-			kinInfo[0][0] = "cosmoID";
-			kinInfo[0][1] = "" + cosmoID;
-			kinInfo[1][0] = "kinID";
-			kinInfo[1][1] = "";
-			try {
-				rs.next();
-				//get the kinID
-				kinInfo[1][1] = rs.getString(1);
-				
-				
-				System.out.println("KIN ID = " + kinInfo[1][1]);
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			if(!(kinInfo[1][1].equals("") || kinInfo[1][1].equals(null)))
+			//check if the info given is valid
+			msg = validateKinInformation(firstName,
+					lastName, address, city, postalCode, homePhone, workPhone);
+			//if the information given is valid
+			if(msg.equals(null) || msg.equals(""))
 			{
-				//update the participant record
-				db.update(kinInfo, "Participant", kinInfo[0][1]);
+				//insert the new record into the database
+				db.insert(values, "Kin");
+				
+				//create the string to put into the WHERE clause
+				whereStmt = this.createWhereStatement(values, 2);
+				
+				//check for the kinID of the new record
+				rs = db.select("kinID", "Kin", whereStmt, "");
+				
+				//the information to update the participant table with
+				kinInfo = new String[2][2];
+				kinInfo[0][0] = "cosmoID";
+				kinInfo[0][1] = "" + cosmoID;
+				kinInfo[1][0] = "kinID";
+				kinInfo[1][1] = "";
+				try {
+					rs.next();
+					//get the kinID
+					kinInfo[1][1] = rs.getString(1);
+					
+					
+					System.out.println("KIN ID = " + kinInfo[1][1]);
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				if(!(kinInfo[1][1].equals("") || kinInfo[1][1].equals(null)))
+				{
+					//update the participant record
+					db.update(kinInfo, "Participant", kinInfo[0][1]);
+				}
 			}
 		}
 		
 		//disconnect from the database
 		db.disconnect();
+		
+		//return any error messages
+		return msg;
 	}
 	
 	/**
@@ -429,14 +396,17 @@ public class ParticipantUpdateFormController
 	 * @param postalCode - caregivers postal code
 	 * @param homePhone - caregivers home phone number
 	 * @param workPhone - caregivers work phone number
+	 * @return - a String containing any error messages produced during
+	 * 	data validation process.
+	 * @author Breanna Wilson CST215
 	 */
-	public void saveCaregiverInformation(String firstName, String lastName,
+	public String saveCaregiverInformation(String firstName, String lastName,
 			String address, String city, String postalCode, String homePhone, 
 			String workPhone)
 	{
 		//connect to the database
 		db.connect();
-		
+		String msg = "";
 		//the array of values to check against in the database
 		String[][] values = new String[7][2];
 		
@@ -482,43 +452,52 @@ public class ParticipantUpdateFormController
 		//if the caregiverID is empty/null
 		if(caregiverID.equals("") || caregiverID.equals(null) || caregiverID.equals(" "))
 		{
-			//insert the caregiver into the database
-			db.insert(values, "Caregiver");
+			msg = validateCaregiverInformation(firstName, 
+					lastName, address, city, postalCode, homePhone, workPhone);
 			
-			//query the database for the new caregiver's ID
-			rs = db.select("caregiverID", "Caregiver", whereStmt, "");
-			
-			try {
-				if(rs.next())
-				{
-					//get the caregiverID
-					caregiverID = rs.getString(1);
-					
-					//the information to update the participant record
-					String[][] caregiverInfo = new String[2][2];
-					caregiverInfo[0][0] = "cosmoID";
-					caregiverInfo[0][1] = "" + cosmoID;
-					caregiverInfo[1][0] = "caregiverID";
-					caregiverInfo[1][1] = caregiverID;
-					
-					//update the participant record
-					db.update(caregiverInfo, "Participant", caregiverInfo[0][1]);
-				}
-				else
-				{
-					//something went wrong
-					System.out.println("ERROR COULD NOT UPDATE");
-				}
+			if(msg.equals(null) || msg.equals(""))
+			{
+				//insert the caregiver into the database
+				db.insert(values, "Caregiver");
 				
-			} catch (SQLException e) {
-				e.printStackTrace();
+				//query the database for the new caregiver's ID
+				rs = db.select("caregiverID", "Caregiver", whereStmt, "");
+				
+				try {
+					if(rs.next())
+					{
+						//get the caregiverID
+						caregiverID = rs.getString(1);
+						
+						//the information to update the participant record
+						String[][] caregiverInfo = new String[2][2];
+						caregiverInfo[0][0] = "cosmoID";
+						caregiverInfo[0][1] = "" + cosmoID;
+						caregiverInfo[1][0] = "caregiverID";
+						caregiverInfo[1][1] = caregiverID;
+						
+						//update the participant record
+						db.update(caregiverInfo, "Participant", caregiverInfo[0][1]);
+					}
+					else
+					{
+						//something went wrong
+						System.out.println("ERROR COULD NOT UPDATE");
+					}
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			
 			}
-			
-			
+		
+		
 		}
 		
 		//disconnect from the database
 		db.disconnect();
+				
+		return msg;
 	}
 	
 	/**
@@ -528,13 +507,16 @@ public class ParticipantUpdateFormController
 	 * @param firstName - emergency contacts first name
 	 * @param lastName - emergency contacts last name
 	 * @param phone - emergency contacts phone number
+	 * @return - a String containing any error messages produced during
+	 * 	data validation process.
+	 * @author Breanna Wilson CST215
 	 */
-	public void saveEmergencyContactInformation(String firstName, String lastName,
+	public String saveEmergencyContactInformation(String firstName, String lastName,
 			String phone)
 	{
 		//connect to the database
 		db.connect();
-		
+		String msg = "";
 		//the array of values to be checked against
 		String[][] values = new String[3][2];
 		
@@ -567,37 +549,44 @@ public class ParticipantUpdateFormController
 		//if the emergency contact record does not currently exist
 		if(ecID.equals("") || ecID.equals(null))
 		{
-			//insert the new emergency contact record into the database
-			db.insert(values, "EmergencyContact");
-		
-			rs = db.select("emergencyContactID", "EmergencyContact", whereStmt, "");
+			msg = validateEmergencyContactInforamtion(firstName, lastName, phone);
 			
-			try {
-				if(rs.next())
-				{
-					//get the emergencyContactID
-					ecID = rs.getString(1);
+			if(msg.equals(null) || msg.equals(""))
+			{
+				//insert the new emergency contact record into the database
+				db.insert(values, "EmergencyContact");
+			
+				rs = db.select("emergencyContactID", "EmergencyContact", whereStmt, "");
+				
+				try {
+					if(rs.next())
+					{
+						//get the emergencyContactID
+						ecID = rs.getString(1);
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
 				}
-			} catch (SQLException e) {
-				e.printStackTrace();
+				
+				//the array of values to be updated in the
+				//participant record
+				values = new String[2][2];
+				
+				values[0][0] = "cosmoID";
+				values[0][1] = "" + cosmoID;
+				values[1][0] = "emergencyContactID";
+				values[1][1] = ecID;
+				
+				//update the participant record
+				db.update(values, "Participant", values[0][1]);
 			}
-			
-			//the array of values to be updated in the
-			//participant record
-			values = new String[2][2];
-			
-			values[0][0] = "cosmoID";
-			values[0][1] = "" + cosmoID;
-			values[1][0] = "emergencyContactID";
-			values[1][1] = ecID;
-			
-			//update the participant record
-			db.update(values, "Participant", values[0][1]);
 		
 		}
 		
 		//disconnect from the database
 		db.disconnect();
+		
+		return msg;
 	}
 	
 	/**
@@ -607,6 +596,7 @@ public class ParticipantUpdateFormController
 	 * @param length - the length of the first dimension of the
 	 * array
 	 * @return - the string to put into the WHERE clause
+	 * @author Breanna Wilson CST215
 	 */
 	private String createWhereStatement(String[][] values, int length)
 	{
@@ -642,6 +632,262 @@ public class ParticipantUpdateFormController
 		}
 		
 		return whereStmt;
+	}
+	
+	/**
+	 * Purpose: Will validate that a phone number
+	 * is in the format(s):
+	 * XXX-XXX-XXXX
+	 * XXX-XXXX
+	 * X-XXX-XXX-XXXX
+	 * @param phone - the phone number to be validated
+	 * @return - true if valid, else false
+	 * @author Breanna Wilson CST215
+	 */
+	private boolean validatePhoneNumber(String phone)
+	{
+		return phone.matches( "(\\d-)?(\\d{3}-)?\\d{3}-\\d{4}") ||
+				phone.equals(null) || phone.isEmpty();
+	}
+	
+	/**
+	 * Purpose: Will validate that a postal code
+	 * is in the proper format A1A 1A1.
+	 * @param postalCode - the postal code to be validated
+	 * @return - true if valid, else false
+	 * @author Breanna Wilson CST215
+	 */
+	private boolean validatePostalCode(String postalCode)
+	{
+		return postalCode.matches("^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$") ||
+				postalCode.equals(null) || postalCode.isEmpty();
+	}
+	
+	/**
+	 * Purpose: Will validate that an address is in the format:
+	 * 111 Fake St
+	 * @param address - the address to be validated
+	 * @return - true if valid, else false.
+	 * @author Breanna Wilson CST215
+	 */
+	private boolean validateAddress(String address)
+	{
+		return address.matches("\\d+\\s+(([a-zA-Z])+|([a-zA-Z]+\\s+[a-zA-Z]+))\\s+[a-zA-Z]*") ||
+				address.equals(null) || address.isEmpty();
+	}
+	
+	/**
+	 * Purpose: Will validate that a city name contains
+	 * only letters and whitespace characters.
+	 * @param city - City name to be validated
+	 * @return - true if valid, else false
+	 * @author Breanna Wilson CST215
+	 */
+	private boolean validateCity(String city)
+	{
+		return city.matches( "([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)" ) ||
+				city.equals(null) || city.isEmpty();
+	}
+	
+	/**
+	 * Purpose: Will validate that a name only contains
+	 * letters, dashes, and periods.
+	 * @param name - The name to be validated
+	 * @return - True if valid, else false
+	 * @author Breanna Wilson CST215
+	 */
+	private boolean validateName(String name)
+	{
+		return name.matches("^[a-zA-Z \\-\\.\\']*$");
+	}
+	
+	/**
+	 * Purpose: Validate all fields for kin information
+	 * that they are in the correct format.
+	 * @param firstName - Kins first name
+	 * @param lastName - Kins last name
+	 * @param address - Kins address
+	 * @param city - Kins city
+	 * @param postalCode - Kins postal code
+	 * @param homePhone - Kins home phone number
+	 * @param workPhone - Kins work phone number
+	 * @return - String with all error messages. If the string
+	 * is empty, all fields are valid.
+	 * @author Breanna Wilson CST215
+	 */
+	private String validateKinInformation(String firstName, String lastName, String address,
+			String city, String postalCode, String homePhone, String workPhone)
+	{
+		String message = "";
+		
+		//if there is no first or last name, kin information is invalid.
+		if(((firstName == null || firstName.equals("")) ||
+				(lastName == null || lastName.equals("")))
+				&& !(address == null || address.equals("") ||
+						city == null || city.equals("") ||
+						postalCode == null || postalCode.equals("") ||
+						homePhone == null || homePhone.equals("") ||
+						workPhone == null || workPhone.equals("")))
+		{
+			message += "Kin must have first and last name.\n";
+		}
+		else if(!(firstName == null || firstName.equals("")) &&
+				!(lastName == null || lastName.equals("")) &&
+				!(address == null || address.equals("")) &&
+				!(city == null || city.equals("")) &&
+				!(postalCode == null || postalCode.equals("")) &&
+				!(homePhone == null || homePhone.equals("")) &&
+				!(workPhone == null || workPhone.equals("")))
+		{
+			//check first name is valid
+			if(!validateName(firstName))
+			{
+				message += "Kin first name is invalid.\n";
+			}
+			
+			//check last name is valid
+			if(!validateName(lastName))
+			{
+				message += "Kin last name is invalid.\n";
+			}
+			
+			//check address is valid
+			if(!validateAddress(address))
+			{
+				message += "Kin address is invalid.\n";
+			}
+			
+			//check city is valid
+			if(!validateCity(city))
+			{
+				message += "Kin city in invalid.\n";
+			}
+			
+			//check postal code is valid
+			if(!validatePostalCode(postalCode))
+			{
+				message += "Kin postal code is invalid.\n";
+			}
+			
+			//check home phone number is valid
+			if(!validatePhoneNumber(homePhone))
+			{
+				message += "Kin home phone number is invalid.\n";
+			}
+			
+			//check work phone number is valid
+			if(!validatePhoneNumber(workPhone))
+			{
+				message += "Kin work phone number is invalid.\n";
+			}
+		}
+		
+		
+		return message;
+	}
+	
+	/**
+	 * Purpose: Validate that all caregiver information 
+	 * is in the correct format.
+	 * @param firstName - the first name of the caregiver
+	 * @param lastName - the last name of the caregiver
+	 * @param address - the address of the caregiver
+	 * @param city - the city for the caregiver
+	 * @param postalCode - the caregivers postal code
+	 * @param homePhone - the caregivers home phone number
+	 * @param workPhone - the caregivers work phone number
+	 * @return - Any error messages. Will be empty if there
+	 * are no errors.
+	 * @author Breanna Wilson CST215
+	 */
+	private String validateCaregiverInformation(String firstName, String lastName,
+			String address, String city, String postalCode, String homePhone, 
+			String workPhone)
+	{
+		String message = "";
+		
+		//if the first or last name fields are empty, entire caregiver area
+		//is invalid. 
+		
+		if((firstName == null || firstName.equals("") ||
+				lastName == null || lastName.equals("")) &&
+				!((address == null || address.equals("")) &&
+					(city == null || city.equals("")) &&
+					(postalCode == null || postalCode.equals("")) &&
+					(homePhone == null || homePhone.equals("")) &&
+					(workPhone == null || workPhone.equals(""))))
+		{
+			message += "Caregiver first and last name must be provided.\n";
+		}
+		else if(!(firstName == null || firstName.equals("")) &&
+				!(lastName == null || lastName.equals("")) &&
+				!(address == null || address.equals("")) &&
+				!(city == null || address.equals("")) &&
+				!(postalCode == null || postalCode.equals("")) &&
+				!(homePhone == null || homePhone.equals("")) &&
+				!(workPhone == null || workPhone.equals("")))
+		{
+			if(! validateName(firstName))
+			{
+				message += "Caregiver first name is invalid.\n";
+			}
+			
+			if(!validateName(lastName))
+			{
+				message += "Caregiver last name is invalid.\n";
+			}
+			
+			if(!validateAddress(address))
+			{
+				message += "Caregiver address is invalid.\n";
+			}
+			
+			if(!validateCity(city))
+			{
+				message += "Caregiver city is invalid.\n";
+			}
+			
+			if(!validatePostalCode(postalCode))
+			{
+				message += "Caregiver postal code is invalid.\n";
+			}
+			
+			if(!validatePhoneNumber(homePhone))
+			{
+				message += "Caregiver home phone number is invalid.\n";
+			}
+			
+			if(!validatePhoneNumber(workPhone))
+			{
+				message += "Caregiver work phone number is invalid.\n";
+			}
+		}
+		
+		return message;
+	}
+	
+	/**
+	 * Purpose: Validates the emergency contact information, and that
+	 * it is in the correct format.
+	 * @param firstName - the first name of the emergency contact
+	 * @param lastName - the last name of the emergency contact
+	 * @param phone - phone number for the emergency contact
+	 * @return - Any error messages. Will be empty if there
+	 * are no errors.
+	 * @author Breanna Wilson CST215
+	 */
+	private String validateEmergencyContactInforamtion(String firstName, String lastName,
+			String phone)
+	{
+		String message = "";
+		
+		if(firstName == null || firstName.equals("") || lastName == null
+				|| lastName.equals("")|| phone == null || phone.equals(""))
+		{
+			message += "All fields for Emergency Contact information must be filled in.\n";
+		}
+		
+		return message;
 	}
 	
 }
