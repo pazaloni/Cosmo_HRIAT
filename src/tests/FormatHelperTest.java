73475b6eb433128926330f0da88e69d05fe58b8d
nneijmeijer <neijmeijer0593@saskpolytech.ca>
2016-03-11 12:17:44 -0600

    Preparing to demo

---
+++ src/tests/FormatHelperTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import static org.junit.Assert.*;
+import helpers.FormatHelper;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FormatHelperTest
+{
+    FormatHelper testHelper;
+    String test1;
+    String test2;
+    String test3;
+    String test4;
+    String test5;
+    @Before
+    public void setUp() throws Exception
+    {
+        testHelper = new FormatHelper();
+        test1 = "1234567890";
+        test2 = "123456789";
+        test3 = "(123)456-7890";
+        test4 = "12f3f4f5fh6k7k8k9l0";
+        test5 = "123456789011";
+    }
+
+    /**
+     * Method f
+     */
+    @Test
+    public void testFormatPhoneNum()
+    {
+        assertTrue(testHelper.formatPhoneNum(test1).equals("(123) 456-7890"));
+        assertFalse(testHelper.formatPhoneNum(test2).equals("(123) 456-7890"));
+        assertTrue(testHelper.formatPhoneNum(test3).equals("(123) 456-7890"));
+        assertTrue(testHelper.formatPhoneNum(test4).equals("(123) 456-7890"));
+        assertFalse(testHelper.formatPhoneNum(test5).equals("(123) 456-7890"));
+    }
+
+}
