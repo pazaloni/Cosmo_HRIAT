35a08fade42ed04ba50c72b0c3db57af89d32b39
Palchinski <cst209@L239B03.cst.siast.sk.ca>
2016-04-12 17:50:06 -0600

    Commented the Incident and IncidentTableViewController

--- src/core/Incident.java
+++ src/core/Incident.java
@@ -3,15 +3,30 @@ package core;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+/**
+ * Purpose: Represents a incident report        
+ * @author Steven Palchinski
+ * @version 1.0
+ *
+ */
 public class Incident
 {
 
+    //Simple strings to hold column info
     private SimpleStringProperty reportID;
     private SimpleStringProperty cosmoID;
     private SimpleStringProperty dateOfIncident;
     private SimpleStringProperty timeOfIncident;
     private SimpleStringProperty incidentDescription;
     
+    /**
+     * Purpose: Constructor for the incident object
+     * @param reportID The id for the incident
+     * @param cosmoID  The cosmo ID of the participant attached to the incident
+     * @param dateOfIncident    date of the incident
+     * @param timeOfIncident    time of the incident
+     * @param incidentDescription   Description of hte incident
+     */
     public Incident( String reportID, String cosmoID, String dateOfIncident, 
             String timeOfIncident, String incidentDescription)
     {
@@ -22,58 +37,59 @@ public class Incident
         this.incidentDescription = new SimpleStringProperty(incidentDescription);
     }
 
+    /**
+     * Purpose: get the incident ID
+     * @return the reportID 
+     */
     public String getIncidentID()
     {
         return reportID.get();
     }
     
+    /**
+     * Purpose: get the report ID string property
+     * @return report ID String property
+     */
     public StringProperty getReportID()
     {
         return reportID;
     }
     
+    /**
+     * Purpose: get the cosmoID string property
+     * @return  cosmoID string property
+     */
     public StringProperty getCosmoID()
     {
         return cosmoID;
     }
     
+    /**
+     * Purpose: Get the date of Incident String property
+     * @return  The dateOfIncident string property
+     */
     public StringProperty getDateOfIncident()
     {
         return dateOfIncident;
     }
     
+    /**
+     * Purpose: Get the time of incident string property
+     * @return  the timeOfIncident string porperty
+     */
     public StringProperty getTimeOfIncident()
     {
         return timeOfIncident;
     }
     
+    /**
+     * Purpose: Get the incident description string property
+     * @return  the incident description string property
+     */
     public StringProperty getIncidentDescription()
     {
         return incidentDescription;
     }
 
-    public StringProperty incidentIDProperty()
-    {
-        return reportID;
-    }
 
-    public StringProperty cosmoIDProperty()
-    {
-        return cosmoID;
-    }
-
-    public StringProperty dateOfIncidentIDProperty()
-    {
-        return dateOfIncident;
-    }
-
-    public StringProperty timeOfIncidentIDProperty()
-    {
-        return timeOfIncident;
-    }
-
-    public StringProperty descriptionProperty()
-    {
-        return incidentDescription;
-    }
 }
